8538fbd3e1b398aae0e65646ae11276071f14d81
Cameron Lee <camdlee18@gmail.com>
2023-02-08 22:19:41 -0500

    first commit

---
+++ hw_module.py
@@ -0,0 +1,8 @@
+# Square Footage Calculation
+def area_calc(length, width):
+    return length*width
+
+# Circumference calculation
+from math import pi
+def circumf_calc(radius):
+    return 2*pi*radius
